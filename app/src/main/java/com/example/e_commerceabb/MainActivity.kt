8f3073e432e1ffe82251d1fe002b02ba1cec19ca
bagirovaayten <ayten.bagirova58@gmail.com>
2023-03-18 16:28:12 +0400

    branch-1 : Add bottom_menu_nav

--- app/src/main/java/com/example/e_commerceabb/MainActivity.kt
+++ app/src/main/java/com/example/e_commerceabb/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.e_commerceabb
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.e_commerceabb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val host: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment?
+                ?: return
+        val navController = host.navController
+        window.statusBarColor = applicationContext.getColor(R.color.transparent)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        binding.bottomNav.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            changeVisibilityOfBottomMenu(destination.id)
+        }
+    }
+
+    private fun changeVisibilityOfBottomMenu(destinationId: Int) {
+        when (destinationId) {
+            R.id.onboardingFragment, R.id.aboutAppFragment, R.id.welcomeFragment ->
+                binding.bottomNav.isVisible = false
+            else -> binding.bottomNav.isVisible = true
+        }
     }
 }
+
