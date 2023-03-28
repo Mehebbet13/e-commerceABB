4f6bb7fb4cedf3bd6334d184668f465ddf7e93bb
bagirovaayten <ayten.bagirova58@gmail.com>
2023-03-28 21:55:14 +0400

    branch-3 : Add nullable

--- app/src/main/java/com/example/e_commerceabb/models/ProductDetailResponse.kt
+++ app/src/main/java/com/example/e_commerceabb/models/ProductDetailResponse.kt
@@ -1,39 +1,40 @@
 package com.example.e_commerceabb.models
+
 import com.google.gson.annotations.SerializedName
 
 data class ProductDetailResponse(
     @SerializedName("enabled")
-    val enabled: Boolean,
+    val enabled: Boolean? = null,
     @SerializedName("imageUrls")
-    val imageUrls: List<String>,
+    val imageUrls: List<String>? = null,
     @SerializedName("quantity")
-    val quantity: Int,
+    val quantity: Int? = null,
     @SerializedName("_id")
-    val id: String,
+    val id: String? = null,
     @SerializedName("name")
-    val name: String,
+    val name: String? = null,
     @SerializedName("description")
-    val description: String,
+    val description: String? = null,
     @SerializedName("currentPrice")
-    val currentPrice: Int,
+    val currentPrice: Int? = null,
     @SerializedName("previousPrice")
-    val previousPrice: Int,
+    val previousPrice: Int? = null,
     @SerializedName("categories")
-    val categories: String,
+    val categories: String? = null,
     @SerializedName("color")
-    val color: String,
+    val color: String? = null,
     @SerializedName("productUrl")
-    val productUrl: String,
+    val productUrl: String? = null,
     @SerializedName("brand")
-    val brand: String,
+    val brand: String? = null,
     @SerializedName("myCustomParam")
-    val myCustomParam: String,
+    val myCustomParam: String? = null,
     @SerializedName("itemNo")
-    val itemNo: String,
+    val itemNo: String? = null,
     @SerializedName("date")
-    val date: String,
+    val date: String? = null,
     @SerializedName("__v")
-    val v: Int,
+    val v: Int? = null,
     @SerializedName("oneMoreCustomParam")
     val oneMoreCustomParam: OneMoreCustomParam
 )
